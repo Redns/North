9f504a29b2ff172f9e253135af8bb1a444737955
Redns <69022576+Redns@users.noreply.github.com>
2022-03-28 21:19:09 +0800

    完善多线程上传

---
+++ Pages/Pics.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ImageBed.Pages
+{
+    public class PicsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
