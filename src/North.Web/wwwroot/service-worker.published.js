96bdcaf50191b53b9313196eb0849ef7503dcc7f
Redns <ljing431@163.com>
2026-04-05 13:04:05 +0800

    [fix] 修复了 Cloudflare Pages 刷新后无法访问网页的问题

--- src/North.Web/wwwroot/service-worker.published.js
+++ src/North.Web/wwwroot/service-worker.published.js
@@ -8,8 +8,8 @@ self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
-const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/, /\.webmanifest$/ ];
-const offlineAssetsExclude = [ /^service-worker\.js$/ ];
+const offlineAssetsInclude = [/\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/];
+const offlineAssetsExclude = [/^service-worker\.js$/];
 
 // Replace with your base path if you are hosting on a subfolder. Ensure there is a trailing '/'.
 const base = "/";
@@ -49,6 +49,15 @@ async function onFetch(event) {
         const request = shouldServeIndexHtml ? 'index.html' : event.request;
         const cache = await caches.open(cacheName);
         cachedResponse = await cache.match(request);
+
+        if (cachedResponse && cachedResponse.redirected) {
+            cachedResponse = new Response(cachedResponse.body,
+                {
+                    headers: cachedResponse.headers,
+                    status: cachedResponse.status,
+                    statusText: cachedResponse.statusText
+                });
+        }
     }
 
     return cachedResponse || fetch(event.request);
